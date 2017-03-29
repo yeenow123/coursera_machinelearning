942b09919874a2efeca8a1c254eb55667a150da1
Yee-Ning Cheng <ycheng@tremorvideo.com>
2017-03-29 17:45:31 -0400

    Exercise 4 completion

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -62,12 +62,40 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Create 5000 x 10 as new Y converted
+I = eye(num_labels);
+Y = zeros(m, num_labels);
+for i=1:m
+  Y(i, :)= I(y(i), :);
+end
 
+a1 = [ones(m, 1) X];
+z2 = a1 * Theta1';
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)]; % 5000 x 26
+z3 = a2 * Theta2'; 
+a3 = sigmoid(z3); % 5000 * 10
 
+delta3 = a3 - Y;
 
+g_z2 = sigmoidGradient(z2);
+delta2 = (delta3 * Theta2) .* [ones(m, 1) g_z2];
 
+unbiased_Theta1 = [zeros(size(Theta1, 1), 1) Theta1(:, 2:end)];
+unbiased_Theta2 = [zeros(size(Theta2, 1), 1) Theta2(:, 2:end)];
 
+Theta1_grad = (1 / m) * (delta2(:, 2:end)' * a1) + ((lambda / m) * unbiased_Theta1);
+Theta2_grad = (1 / m) * (delta3' * a2) + ((lambda / m) * unbiased_Theta2);
 
+first = -Y .* log(a3);
+second = (1 - Y) .* log(1 - a3);
+cost = first - second;
+total = sum(cost);
+
+reg = (lambda / (2 * m)) * (sum(sum(Theta1(:, 2:end) .^ 2)) + sum(sum(Theta2(:, 2:end) .^ 2)))
+
+J = (sum(total) / m) + reg
+
+    
 
 
 
--- machine-learning-ex4/ex4/sigmoidGradient.m
+++ machine-learning-ex4/ex4/sigmoidGradient.m
@@ -12,7 +12,9 @@ g = zeros(size(z));
 % Instructions: Compute the gradient of the sigmoid function evaluated at
 %               each value of z (z can be a matrix, vector or scalar).
 
+sig = 1.0 ./ (1.0 + exp(-z));
 
+g = sig .* (1 - sig);
 
 
 
